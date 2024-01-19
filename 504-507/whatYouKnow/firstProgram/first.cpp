650354fc24063b89a09fee8b5abd21e13d663dfa
jmichaelmoore <jmichael@tamu.edu>
2024-01-19 10:36:43 -0600

    1/19 9:10

---
+++ 504-507/whatYouKnow/firstProgram/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//using namespace std;
+
+using std::cout, std::cin, std::endl, std::string;
+
+int main () {
+    std::string name = "";
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << name << ", welcome to CSCE 120!" << endl;
+}
