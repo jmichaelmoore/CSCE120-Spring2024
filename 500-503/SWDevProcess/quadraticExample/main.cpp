386616e98447defff72145c3b5e2062d0dc31049
Michael Moore <jmichael@tamu.edu>
2024-02-07 11:24:02 -0600

    lecture 2/7

--- 500-503/SWDevProcess/quadraticExample/main.cpp
+++ 500-503/SWDevProcess/quadraticExample/main.cpp
@@ -41,3 +41,75 @@ int main() {
 
   
 }
+
+void outputEquation(double a, double b, double c) {
+    if (a != 0) {
+        cout << a << "x^2 + ";
+    }
+    cout << b << "x + ";
+    cout << c << " = 0" << endl;
+}
+
+void processLinear(double b, double c) {
+    cout << "x = " << (-c/b) << endl;
+}
+
+void processQuadratic(double a, double b, double c) {
+    /*
+    Assign disc to b*b - 4*a*c
+````If disc is negative
+        processImaginary
+    Else (includes 0)
+        processReal
+    */
+   double disc = b*b - 4*a*c;
+   if (disc < 0) {
+    processImaginary(disc, a, b, c);
+   }
+   else { // disc >= 0
+    processReal(disc, a, b, c);
+   }
+}
+
+void processReal(double disc, double a, double b, double c) {
+    /*Assign x1 to (b*b + sqrt(disc))/(2*a)
+Output “x = “x1
+If disc not equal to 0
+Assign x2 to  (b*b - sqrt(disc))/(2*a)
+Output “x = “x2
+*/
+    double x1 = (b*b + sqrt(disc)/(2*a));
+    cout << "x = " << x1 << endl;
+    if (disc != 0) {
+        double x2 = (b*b - sqrt(disc)/(2*a));
+        cout << "x = " << x2 << endl;
+    }
+}
+
+void processImaginary(double disc, double a, double b, double c) {
+    /*Assign disc to disc*-1
+Assign x1real to b*b/(2*a)
+Assign x1img to sqrt(disc)/(2*a)
+Output “x = “x1real” + “x1img”i”
+Output “x = “x1real” - “x1img”i”
+*/
+    disc = fabs(disc);
+    double real = b*b/(2*a);
+    double img = sqrt(disc)/(2*a);
+    cout << "x = " << real << " + " << img << "i" << endl;
+    cout << "x = " << real << " - " << img << "i" << endl;
+
+
+}
+
+bool isSolution(double a, double b, double c, double xReal, double xImag) {
+    double real = a*xReal*xReal - a*xImag*xImag + b*xReal + c;
+    double imag = -2*a*xReal*xImag - b*xImag;
+    if (fabs(real + imag) < 0.00000001) {
+        cout << "is a solution";
+    }
+    else {
+        cout << "not a soluction";
+    }
+    cout << endl;
+}
--- 504-507/SWDevProcess/quadraticExample/main.cpp
+++ 504-507/SWDevProcess/quadraticExample/main.cpp
@@ -41,3 +41,85 @@ int main() {
 
   
 }
+
+void outputEquation(double a, double b, double c) {
+    if (a != 0) {
+        cout << a << "x^2 + ";
+    }
+    cout << b << "x + ";
+    cout << c << " = 0" << endl;
+}
+
+void processQuadratic(double a, double b, double c) {
+    double disc = b*b - 4*a*c;
+    if (disc < 0) {
+        processImaginary(disc, a, b, c);
+    }
+    else {  // >= 0
+        processReal(disc, a, b, c);
+    }
+}
+
+void processImaginary(double disc, double a, double b, double c) {
+    /*Assign disc to disc*-1
+Assign real to -b/(2*a)
+Assign imag to sqrt(disc)/(2*a)
+Output “x = “real” - “imag”i”
+Output “x = “real” + “imag”i”
+*/
+    disc = fabs(disc);
+    double real = -b/(2*a);
+    double imag = sqrt(disc)/(2*a);
+    cout << "x = " << real << " - " << imag << "i" << endl;
+    cout << "x = " << real << " + " << imag << "i" << endl;
+    if (isSolution(a, b, c, real, imag)) {
+        cout << "valid solution" << endl;
+    }
+    else {
+        cout << "invalid solution" << endl;
+    }
+}
+
+void processReal(double disc, double a, double b, double c) {
+    /*Assign x1 to (-b - sqrt(disc))/(2*a)
+Output “x = “x1
+If disc not equal 0
+Assign x2 to  (-b + sqrt(disc))/(2*a)
+Output “x = “x2
+*/
+    double x1 = (-b - sqrt(disc)/(2*a));
+    cout << "x = " << x1 << endl;
+    if (isSolution(a, b, c, x1, 0)) {
+        cout << "valid solution" << endl;
+    }
+    else {
+        cout << "invalid solution" << endl;
+    }
+    if (disc != 0) {
+        double x2 = (-b + sqrt(disc)/(2*a));
+        cout << "x = " << x2 << endl;
+        if (isSolution(a, b, c, x2, 0)) {
+            cout << "valid solution" << endl;
+        }
+        else {
+            cout << "invalid solution" << endl;
+        }
+    }
+}
+
+void processLinear(double b, double c) {
+    cout << "x = " << -c/b << endl;
+}
+
+bool isSolution(double a, double b, double c, double xReal, double xImag) {
+    double real = a*xReal*xReal - a*xImag*xImag + b*xReal + c;
+    double imag = -2*a*xReal*xImag - b*xImag;
+    double sum = real + imag;
+    cout << "sum: " << sum << endl;
+    if (abs(sum) < 0.000000001) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
