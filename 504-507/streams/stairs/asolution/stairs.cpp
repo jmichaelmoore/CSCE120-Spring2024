a2226334a1abafb62c18d937bc27db657dce1280
Michael Moore <jmichael@tamu.edu>
2024-02-19 09:55:31 -0600

    lecture 02/16

--- 504-507/streams/stairs/asolution/stairs.cpp
+++ 504-507/streams/stairs/asolution/stairs.cpp
@@ -8,6 +8,22 @@ int main()
     cout << "Please provide a number of lines to print the stair-case pattern : ";
     unsigned int no_lines;
     cin >> no_lines;
+
+    while (cin.fail() || no_lines > 80) {
+        if (!cin.fail()) { // failed due to no_lines being incorrect
+            cout << "number must be <= 80" << endl;
+        }
+        if (cin.bad()) { // check first since bad also makes fail true
+            cout << "unexpected error, please try again." << endl;
+        }
+        else if (cin.fail()) { // check for invalid char for int
+            cout << "your input is not a valid number" << endl;
+        }
+        cin.clear(); // reset bits
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // remove bad data
+        cout << "Please provide a number of lines to print the stair-case pattern : ";
+        cin >> no_lines;
+    }
     
     for (unsigned int i = 1; i <= no_lines ; ++i) {
         for (unsigned int j = 1 ; j <= i ; ++j) {
