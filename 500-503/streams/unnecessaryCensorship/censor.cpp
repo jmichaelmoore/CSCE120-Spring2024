ff258c389343cec605dd948651aaa1188c9050fc
Michael Moore <jmichael@tamu.edu>
2024-02-21 09:58:34 -0600

    lecture 02/21

--- 500-503/streams/unnecessaryCensorship/censor.cpp
+++ 500-503/streams/unnecessaryCensorship/censor.cpp
@@ -6,6 +6,13 @@
 using std::cin, std::cout, std::cerr, std::endl, std::string;
 using std::istringstream, std::ifstream, std::ofstream;
 
+string tolower(string wd) {
+	for (size_t i=0; i<wd.size(); ++i) {
+		wd.at(i) = tolower(wd.at(i));
+	}
+	return wd;
+}
+
 string getReplace(string wd) {
 	return "***";
 }
@@ -17,12 +24,33 @@ int main() {
 	string censoredTextFilename = "censored.txt";
 
 	// Declare/Define/Open filestreams
+	ifstream censorIn(censorWordsFilename);
+	ifstream origIn(originalTextFilename);
+	ofstream censorOut(censoredTextFilename);
 
 	// make sure all files were opened and give feedback if not
+	if (!censorIn.is_open()) {
+		cout << "Unable to open: " << censorWordsFilename << endl;
+		return 1; // return with error
+	}
+	if (!origIn.is_open()) {
+		cout << "Unable to open: " << originalTextFilename << endl;
+		return 1; // return with error
+	}
+	if (!censorOut.is_open()) {
+		cout << "Unable to open: " << censoredTextFilename << endl;
+		return 1; // return with error
+	}
 
 	// get list of words to censor from file
+	string censorWords;
+	string censorWord;
+	while (censorIn>>censorWord) {
+		censorWords += (" " + tolower(censorWord));
+	}
 
 	// print out list of censored words (for debugging) remove later from final program
+	cout << "censorWords: " << censorWords << endl;
 
 	// read original, process and send to output
 	// process line by line
@@ -30,4 +58,21 @@ int main() {
 		// see if each censor word is in line
 		// if it is there, replace with symbols
 		// output modified line
+	while (!origIn.eof()) {
+		string line;
+		getline(origIn, line);
+		string oLine = line;
+		line = tolower(line);
+		istringstream iCensorW(censorWords);
+		string censorWord;
+		while (iCensorW >> censorWord) { // for each censor word
+			size_t position = line.find(censorWord);
+			while (position != string::npos) {
+				line.replace(position, censorWord.size(), "***");
+				oLine.replace(position, censorWord.size(), "***");
+				position = line.find(censorWord);
+			}
+		}
+		censorOut << line;
+	}
 }
--- 504-507/streams/unnecessaryCensorship/censor.cpp
+++ 504-507/streams/unnecessaryCensorship/censor.cpp
@@ -6,6 +6,13 @@
 using std::cin, std::cout, std::cerr, std::endl, std::string;
 using std::istringstream, std::ifstream, std::ofstream;
 
+string tolower(string wd) {
+	for (size_t i=0; i<wd.size(); ++i) {
+		wd.at(i) = tolower(wd.at(i));
+	}
+	return wd;
+}
+
 string getReplace(string wd) {
 	return "***";
 }
@@ -17,12 +24,33 @@ int main() {
 	string censoredTextFilename = "censored.txt";
 
 	// Declare/Define/Open filestreams
+	ifstream censorIn(censorWordsFilename);
+	ifstream origIn(originalTextFilename);
+	ofstream censorOut(censoredTextFilename);
 
 	// make sure all files were opened and give feedback if not
+	if (!censorIn.is_open()) {
+		cout << "Unable to open " << censorWordsFilename << endl;
+		return 1;
+	}
+	if (!origIn.is_open()) {
+		cout << "Unable to open " << originalTextFilename << endl;
+		return 1;
+	}
+	if (!censorOut.is_open()) {
+		cout << "Unable to open " << censoredTextFilename << endl;
+		return 1;
+	}
 
 	// get list of words to censor from file
+	string censorWords;
+	string censorWord;
+	while (censorIn >> censorWord) {
+		censorWords += (" " + tolower(censorWord));
+	}
 
 	// print out list of censored words (for debugging) remove later from final program
+	cout << "censorWords: " << censorWords << endl;
 
 	// read original, process and send to output
 	// process line by line
@@ -30,4 +58,22 @@ int main() {
 		// see if each censor word is in line
 		// if it is there, replace with symbols
 		// output modified line
+	while (!origIn.eof()) {
+		string line;
+		getline(origIn, line);
+		string lineOut = line;
+		line = tolower(line);
+		istringstream censorWds(censorWords);
+		string censorWd;
+		while (censorWds >> censorWd) { // while there are censor words to process
+			size_t position = line.find(censorWd);
+			while (position != string::npos) {
+				line.replace(position, censorWd.size(), getReplace(censorWd));
+				lineOut.replace(position, censorWd.size(), getReplace(censorWd));
+				position = line.find(censorWd);
+			}
+		}
+		censorOut << lineOut;
+	
+	}
 }
