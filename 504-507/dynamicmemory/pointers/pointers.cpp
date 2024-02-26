fab8511d5736e4e40313a2d1c5ccd36cf8dd11ee
Michael Moore <jmichael@tamu.edu>
2024-02-26 10:24:56 -0600

    update 2/26

--- 504-507/dynamicmemory/pointers/pointers.cpp
+++ 504-507/dynamicmemory/pointers/pointers.cpp
@@ -4,7 +4,7 @@ using std::cout, std::endl;
 
 void printAry(const int ary[], size_t size) {
   // side effect to fix
-  ary[0] = -1;
+  //ary[0] = -1;
 
   for (size_t i=0; i<size; ++i) {
     cout << ary[i] << endl;
@@ -15,16 +15,17 @@ void printAry(const int ary[], size_t size) {
 int main() {
   int i = 4;
   // declare pointer variable k based on i
-  
+  int* k = nullptr;
+  k = &i;
 
   // replace ??? with the address of the variable i
-  cout << "address of i: " << ??? << endl;
+  cout << "address of i: " << &i << endl;
   cout << "value in i:   " << i << endl;
   // replace ??? with the address of the varaible k
-  cout << "address of k: " << ??? << endl;
+  cout << "address of k: " << &k << endl;
   cout << "value in k:   " << k << endl;
   // replace ??? with dereference of k
-  cout << "value in *k:  " << ??? << endl;
+  cout << "value in *k:  " << *k << endl;
 
   int ary[] = {5, 7, 9};
   size_t size = 3;
