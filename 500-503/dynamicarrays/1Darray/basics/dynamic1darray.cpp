f8b3dbb9da76424d01b93ef5239973045e337717
Michael Moore <jmichael@tamu.edu>
2024-02-29 23:45:50 -0600

    prep 03/01

---
+++ 500-503/dynamicarrays/1Darray/basics/dynamic1darray.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stdexcept>
+
+using std::cout, std::cin, std::endl;
+
+double max(double[], unsigned int);
+void printAry(double[], unsigned int);
+
+int main() {
+  cout << "How many values will you enter?";
+  unsigned int size = 0;
+  cin >> size;
+  if (cin.fail() || size == 0) {
+    cout << "You must enter a valid size greater than zero." << endl;
+    return 1; // empty array so nothing to read into
+  }
+  
+  // define pointer to appropriate datatype (double)
+  // allocate memory for the array with "new"
+
+  // initialize
+
+  // use the array - same as using an array on the heap
+
+  for (unsigned int i=0; i<size; ++i) {
+    //cin >> ary[i]; // do this
+    //cin >> *(ary + i); // don't do this
+    //cin >> i[ary]; // don't do this
+  }
+
+  cout << "The max is: " << "replace with call to max" << endl; // add call to max
+  cout << "Numbers: " << endl;
+  //printAry(ary, size);
+  cout << endl;
+  // release memory for the array with "delete[]"
+
+}
+
+double max(double nums[], unsigned int size) {
+  if (size == 0) {
+    throw std::invalid_argument("Cannot find max for an empty array");
+  }
+  double max = nums[0];
+  for (unsigned int i=1; i<size; ++i) { // start at 1 since index 0 already addressed
+    if (nums[i] > max) {
+      max = nums[i];
+    }
+  }
+  return max;
+}
+
+void printAry(double* nums, unsigned int size) {
+  if (size == 0) {
+    cout << endl;
+    return;
+  }
+  for (unsigned int i=0; i<size; ++i) {
+    cout << nums[i] << " ";
+    if ((i+1)%5 == 0) { // why do I use i+1
+      cout << endl;
+    }
+  }  
+}
---
+++ 504-507/dynamicarrays/1Darray/cstring/countletters/cstringptrmath.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout, std::endl;
+
+int main() {
+    // declare cstring with '*' called str with 3 elements
+
+    //str[0] = 'H';
+    //str[1] = 'i';
+    //str[2] = '\0';
+
+    // count characters
+    unsigned int cnt = 0;
+    // set up pointer called iter to move through string and not modify str
+
+    // iterate through string using iter and count as we go
+
+    cout << "cnt: " << cnt << endl;
+    //delete[] str;
+}
