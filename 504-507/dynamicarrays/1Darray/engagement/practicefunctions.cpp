4f3c8483ea9556bd89da88bb8094ce48f7b2bf37
Michael Moore <jmichael@tamu.edu>
2024-03-04 08:28:26 -0600

    fix

--- 504-507/dynamicarrays/1Darray/engagement/practicefunctions.cpp
+++ 504-507/dynamicarrays/1Darray/engagement/practicefunctions.cpp
@@ -1,21 +1,15 @@
 #include "practicefunctions.h"
 
 void makeary(char*& ary, unsigned int size) {
-  ary = new char[size];
+
 }
 
 unsigned int sumary(const char* ary, unsigned int size) {
-  unsigned int value = 0;
-  for (unsigned int i=0; i<size; ++i) {
-    value += ary[i];
-  }
-  return value;
+  
 }
 
 void deleteary(char*& ary, unsigned int& size) {
   // deletes all memory on the heap 
   //  and set ary and size to values representing an empty array
-  delete [] ary;
-  ary = nullptr;
-  size = 0;
+
 }
