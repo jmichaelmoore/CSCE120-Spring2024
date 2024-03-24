be72d02dc970a119f973836f62450ba6ce49a37e
Michael Moore <jmichael@tamu.edu>
2024-03-24 16:48:56 -0500

    prep for 03/25

---
+++ 504-507/classes/basics-constructor/complex/complex.cpp
@@ -0,0 +1,12 @@
+ // define a class complex number
+ //   has a real part and an imaginary part (make them double)
+ // constructors
+ //  - default
+ //  - real and imagingary parameters
+ //  - real parameter
+ //  - imaginary parameter (not possible)
+ //  - Complex parameter
+
+ class Complex {
+public:
+ }
---
+++ 504-507/classes/basics-constructor/pixel/Pixel.cpp
@@ -0,0 +1,8 @@
+#include <stdexcept>
+class Pixel {
+private: // optional here since classes are private by default
+public:
+    //parameterized constructor with r, g, & b values passed in
+    //copy constructor with Pixel passed in
+    //default constructor ? What to set r, g & b values to?
+};
